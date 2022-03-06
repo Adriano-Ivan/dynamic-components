377b58a24cc57b33b8eca319db4e84cb83e4ad7a
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-03-06 13:30:30 -0300

    Variando componentes.

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("Home Page Component was unmounted");
+  },
+  activated() {
+    console.log("Home component activated");
+  },
+  deactivated() {
+    console.log("Home component deactivated");
+  },
+};
+</script>
